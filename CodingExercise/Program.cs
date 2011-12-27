d4ff60afcab25806e8c0dc199e508e93236e43b7
Katherine Cook <katherine.e.cook@gmail.com>
2011-12-27 03:26:44 -0600

    initial project commit

---
+++ CodingExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FizzBuzz;
+
+
+namespace FizzBuzz
+{
+    class Fizz
+    {
+        public static Object[] fizzbuzzfunc(int start, int end)
+        {
+            Object[] myArray = new Object[end+1 - start];
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    myArray[i - start] = "Fizzbuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    myArray[i - start] = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    myArray[i - start] = "Buzz";
+                }
+                else
+                {
+                    myArray[i - start] = i;
+                }
+            }
+            return myArray;
+        }
+    }
+}
+
+namespace CodingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 5;
+            int e = 200;
+            Object[] fizzy = Fizz.fizzbuzzfunc(s,e);
+            foreach (Object elem in fizzy) 
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
---
+++ Tests/test_fizzbuzz.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace Tests
+{
+    /// <summary>
+    /// Summary description for test_fizzbuzz
+    /// </summary>
+    [TestClass]
+    public class test_fizzbuzz
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int s = 0;
+            int e = 100;
+            Object[] fizzy = Fizz.fizzbuzzfunc(s, e);
+            for (int i = 0; i<fizzy.Length-1;i++)
+            {
+                if(i%3==0 && i%5==0)
+                    Assert.AreEqual("Fizzbuzz", fizzy[i]);
+                else if (i % 3 == 0)
+                    Assert.AreEqual("Fizz", fizzy[i]);
+                else if (i % 5 == 0)
+                    Assert.AreEqual("Buzz", fizzy[i]);
+                else
+                    Assert.AreEqual(i, fizzy[i]);
+            }
+        }
+    }
+}
